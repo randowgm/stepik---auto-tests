d22c31be91d11fcec7809d1e242628af32f0e995
Aleksandr Panov <73994020+randowgm@users.noreply.github.com>
2021-01-12 23:04:19 +0300

    Update 14.py

--- 14.py
+++ 14.py
@@ -13,19 +13,12 @@ try:
     link = "http://suninjuly.github.io/explicit_wait2.html"
     browser = webdriver.Chrome()
     browser.get(link)
-
-    # код
-    
-    # говорим Selenium проверять в течение 12 секунд
     
     button = WebDriverWait(browser, 12).until(
         EC.text_to_be_present_in_element((By.ID, "price"), "$100")
     )
     button = browser.find_element_by_css_selector(".btn.btn-primary")
     button.click()
-    
-    
-    #ищем и решаем
     
     element1 = browser.find_element_by_css_selector("#input_value")
     x_element = element1.text
@@ -36,16 +29,10 @@ try:
     input = browser.find_element_by_css_selector("#answer")
     input.send_keys(y)
     
-    
-    # ок
     button2 = browser.find_element_by_css_selector("#solve")
     button2.click()
     
 
-
-
 finally:
-    # ожидание чтобы визуально оценить результаты прохождения скрипта
     time.sleep(10)
-    # закрываем браузер после всех манипуляций
     browser.quit()
